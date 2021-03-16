eccae32edefbfc1c5a58a7b0315d18996478a383
Taner Öksüz <taner.oeksuez@outlook.com>
2021-03-16 14:53:35 +0100

    some erros fixed. aes encryption added into python script.

--- py/generate.py
+++ py/generate.py
@@ -13,7 +13,6 @@ from Crypto.Cipher import AES
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-k","--key_length", help="length of key in bits", type=int)
-parser.add_argument("-d","--data_length", help="length of data in bits", type=int)
 parser.add_argument("-w","--word_number", help="number of words in bits", type=int)
 
 args = parser.parse_args()
@@ -21,21 +20,24 @@ args = parser.parse_args()
 if (args.key_length != 128) and (args.key_length != 192) and (args.key_length != 256):
     print("Key length: ",args.key_length," is invalid!")
     exit(1)
-if (args.data_length != 128) and (args.data_length != 192) and (args.data_length != 256):
-    print("Data length: ",args.data_length," is invalid!")
-    exit(1)
 if (args.word_number is None) or (args.word_number<1):
     print("Word number: ",args.word_number," is invalid!")
     exit(1)
 
-key = secrets.token_hex(int(args.key_length/8))
+key = Random.new().read(int(args.key_length/8))
 f = open("key.txt", "w")
-f.writelines(key+"\n")
+f.writelines(key.hex()+"\n")
 f.close()
 
 
-f = open("data.txt", "w")
+d = open("data.txt", "w")
+e = open("encrypt.txt", "w")
+iv = bytes(bytearray(AES.block_size))
+cipher = AES.new(key,AES.MODE_CBC,iv)
 for i in range(args.word_number):
-    data = secrets.token_hex(int(args.data_length/8))
-    f.writelines(data+"\n")
-f.close()
+    data = Random.new().read(AES.block_size)
+    encrypt = cipher.encrypt(data)
+    d.writelines(data.hex()+"\n")
+    e.writelines(encrypt.hex()+"\n")
+d.close()
+e.close()
